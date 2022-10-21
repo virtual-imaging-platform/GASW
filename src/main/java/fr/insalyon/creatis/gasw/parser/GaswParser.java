87fd89a6d5617e8feaaed1cf987679fd5f24bbd5
Alae Es-saki <alaeessaki@gmail.com>
2022-10-21 15:02:27 +0100

    fixing file url

--- src/main/java/fr/insalyon/creatis/gasw/parser/GaswParser.java
+++ src/main/java/fr/insalyon/creatis/gasw/parser/GaswParser.java
@@ -79,7 +79,7 @@ public class GaswParser extends DefaultHandler {
     private List<String> inputsList;
 
     private static final String LFN_PREFIX = "lfn://";
-    private static final String FILE_PREFIX = "file://";
+    private static final String FILE_PREFIX = "file:/";
 
     public GaswParser() throws GaswException {
 
@@ -131,7 +131,7 @@ public class GaswParser extends DefaultHandler {
                 if (value.contains("lfn:/")) {
                     value = LFN_PREFIX + new URI(value + ".tar.gz").getPath();
                 }else if(value.contains("file:/")){
-                    value = FILE_PREFIX + value + ".tar.gz";
+                    value = FILE_PREFIX + new URI(value + ".tar.gz").getPath();
                 }else {
                     value = LFN_PREFIX + value + ".tar.gz";
                 }
