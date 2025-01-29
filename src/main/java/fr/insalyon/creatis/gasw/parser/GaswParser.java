005711819ea59875da7b2f85a01a062e28d1a5ea
Nicolas Georges <nicolas.georges@creatis.insa-lyon.fr>
2025-01-29 08:05:07 +0100

    GaswParser: refactoring
    Move stripExtensions into addName() to preserve factorization
    of DIR_AND_NAME and NAME cases.

--- src/main/java/fr/insalyon/creatis/gasw/parser/GaswParser.java
+++ src/main/java/fr/insalyon/creatis/gasw/parser/GaswParser.java
@@ -427,7 +427,7 @@ public class GaswParser extends DefaultHandler {
                 case DIR_AND_NAME:
                 {
                     addDir(inputsMap.get(part.getValue()), content);
-                    addName("/", inputsMap.get(part.getValue()), content);
+                    addName("/", inputsMap.get(part.getValue()), content, null);
                 }
                 break;
                 case DIR:
@@ -437,17 +437,7 @@ public class GaswParser extends DefaultHandler {
                 break;
                 case NAME:
                 {
-                    URI u = new URI(inputsMap.get(part.getValue()));
-                    File f = new File(u.getPath());
-                    String basename = f.getName();
-                	if (part.getStripExtensions()!=null) {
-                		for (String extn : part.getStripExtensions()) {
-                            if (basename.endsWith(extn)) {
-                                basename = basename.substring(0, basename.length() - extn.length());
-                    		}
-                    	}
-                	}
-                    content.append(basename);
+                    addName("", inputsMap.get(part.getValue()), content, part.getStripExtensions());
                 }
                 break;
                 case OPTIONS:
@@ -483,13 +473,20 @@ public class GaswParser extends DefaultHandler {
     }
 
     private static void addName(
-        String separator, String s, StringBuilder content)
+        String separator, String uri, StringBuilder content, Set<String> strippedExtensions)
         throws URISyntaxException {
 
-        URI u = new URI(s);
-        File f = new File(u.getPath());
-        if (f.getName().length() > 0) {
-            content.append(separator).append(f.getName());
+        URI u = new URI(uri);
+        String filename = new File(u.getPath()).getName();
+        if (strippedExtensions != null) {
+            for (String extn : strippedExtensions) {
+                if (filename.endsWith(extn)) {
+                    filename = filename.substring(0, filename.length() - extn.length());;
+                }
+            }
+        }
+        if (filename.length() > 0) {
+            content.append(separator).append(filename);
         }
     }
 }
